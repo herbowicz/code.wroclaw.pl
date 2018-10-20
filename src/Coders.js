01a9fe08955246f6faca5ae8fcbf9e6a6c0288bf
herbowicz <herbowicz@gmail.com>
2018-10-21 00:49:26 +0200

    remove disqus

--- src/Coders.js
+++ src/Coders.js
@@ -1,71 +1,83 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import Coder from './Coder'
-import './Coders.css'
+import Coder from "./Coder";
+import "./Coders.css";
 
 class Coders extends Component {
-    
-    state = {
-        coders: [],
-        page: 1,
-        isLoading: false,
-        user: {}
-    }
+  state = {
+    coders: [],
+    page: 1,
+    isLoading: false,
+    user: {}
+  };
 
-    componentDidMount() {
-        this.fetchData()
-    }
+  componentDidMount() {
+    this.fetchData();
+  }
 
-    fetchData = async (page) => {
-        this.setState({ isLoading: true })
+  fetchData = async page => {
+    this.setState({ isLoading: true });
 
-        const response = await fetch(`https://api.github.com/search/users?q=location:Wroclaw+location:Wrocław?&per_page=99&page=${page}`)
-        const coders = await response.json()
-        this.setState({
-            coders: this.state.coders.concat(coders.items)
-        })
-        this.setState({ isLoading: false })
-    }
+    const response = await fetch(
+      `https://api.github.com/search/users?q=location:Wroclaw+location:Wrocław?&per_page=99&page=${page}`
+    );
+    const coders = await response.json();
+    this.setState({
+      coders: this.state.coders.concat(coders.items)
+    });
+    this.setState({ isLoading: false });
+  };
 
-    fetchUser = async (id, index) => {
-        const response = await fetch(`https://api.github.com/users/${id}?access_token=a4f359c6bae38b0d4de0be60aba753a68a09581f`)
-        const user = await response.json()
-        this.setState({ user })
-    }
+  fetchUser = async (id, index) => {
+    const response = await fetch(
+      `https://api.github.com/users/${id}?access_token=a4f359c6bae38b0d4de0be60aba753a68a09581f`
+    );
+    const user = await response.json();
+    this.setState({ user });
+  };
 
-    clear = () => {
-        this.setState({ user: {} })
-    }
+  clear = () => {
+    this.setState({ user: {} });
+  };
 
-    showMore = () => {
-        this.fetchData(this.state.page + 1)
-        this.setState({page: this.state.page + 1})
-    }
+  showMore = () => {
+    this.fetchData(this.state.page + 1);
+    this.setState({ page: this.state.page + 1 });
+  };
 
-    render() {
-        const { coders, user, isLoading } = this.state;
-        return (              
-                    
-            <InfiniteScroll
-                dataLength={coders.length}
-                next={this.showMore}
-                hasMore={true}
-                loader={null}
-            >
-                <div className="list" >
-                    {coders && coders.map((coder, i) =>
-                        <span key={coder.id} onMouseOver={() => this.fetchUser(coder.login)} onMouseLeave={this.clear}>
-                            <Coder coder={coder} user={user} i={i}/>
-                        </span>)}
-                </div>
-                <div>
-                    {isLoading && "Loading..."}
-                    <button onClick={this.showMore}>Show more coders</button>
-                </div>
-            </InfiniteScroll>
-
-        )
-    }
+  render() {
+    const { coders, user, isLoading } = this.state;
+    return (
+      <InfiniteScroll
+        dataLength={coders}
+        next={this.showMore}
+        hasMore={true}
+        loader={null}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <div className="list">
+          {coders &&
+            coders.map((coder, i) => (
+              <span
+                key={coder.id}
+                onMouseOver={() => this.fetchUser(coder.login)}
+                onMouseLeave={this.clear}
+              >
+                <Coder coder={coder} user={user} i={i} />
+              </span>
+            ))}
+        </div>
+        <div>
+          {isLoading && "Loading..."}
+          <button onClick={this.showMore}>Show more coders</button>
+        </div>
+      </InfiniteScroll>
+    );
+  }
 }
 
 export default Coders;
