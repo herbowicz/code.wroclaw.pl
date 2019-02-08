cf22cff3be022278ad2f85f20a92f32c2fc59b70
Greg Herbowicz <herbowicz@gmail.com>
2019-02-08 10:18:37 +0100

    gh warnings fix

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
-import React, { Component } from "react";
-import "./App.css";
-import Coders from "./Coders";
-import Disqus from "./Disqus";
+import React, { Component } from 'react';
+import './App.css';
+import Coders from './Coders';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <div className="main">
-          <header className="header">
-            <svg
-              className="logo"
-              alt="logo"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 100 100"
-              version="1.1"
-            >
-              <g className="group" transform="scale(.1,.1)">
-                <path
-                  className="out"
-                  strokeWidth="20"
-                  fill="none"
-                  strokeLinecap="round"
-                  d="M0,0,l500,288.6751,l0,577.3503,l-300,173.2051,l-200,-115.4701,l-200,115.4701,l-300,-173.2051,l0,-577.3503z"
-                />
-                <path
-                  className="in"
-                  strokeWidth="25"
-                  fill="none"
-                  strokeLinecap="round"
-                  d="M0,115.4701,l100,57.735,l0,461.8802,l100,57.735,l0,-461.8802,l200,115.4701,l0,461.8802,l-200,115.4701,l-200,-115.4701,l-200,115.4701,l-200,-115.4701,l0,-461.8802,l200,-115.4701,l0,461.8802,l100,-57.735,l0,-461.8802z"
-                />
-              </g>
-            </svg>
-          </header>
-          <div className="coders">
-            <Coders />
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <div className="main">
+                    <header className="header">
+                        <svg
+                            className="logo"
+                            alt="logo"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 100 100"
+                            version="1.1">
+                            <g className="group" transform="scale(.1,.1)">
+                                <path
+                                    className="out"
+                                    strokeWidth="20"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    d="M0,0,l500,288.6751,l0,577.3503,l-300,173.2051,l-200,-115.4701,l-200,115.4701,l-300,-173.2051,l0,-577.3503z"
+                                />
+                                <path
+                                    className="in"
+                                    strokeWidth="25"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    d="M0,115.4701,l100,57.735,l0,461.8802,l100,57.735,l0,-461.8802,l200,115.4701,l0,461.8802,l-200,115.4701,l-200,-115.4701,l-200,115.4701,l-200,-115.4701,l0,-461.8802,l200,-115.4701,l0,461.8802,l100,-57.735,l0,-461.8802z"
+                                />
+                            </g>
+                        </svg>
+                    </header>
+                    <div className="coders">
+                        <Coders />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Coder.js
+++ src/Coder.js
@@ -1,90 +1,108 @@
-import React from "react";
-import "./Coder.css";
+import React from 'react';
+import './Coder.css';
 
 const Coder = ({ coder, user, i }) => (
-  <span className="user">
-    {user.login === coder.login && (
-      <div>
-        <div className="top-info1" />
-        <div className="top-info2" />
-        <div className="info">
-          <ul>
-            <h3>
-              <a href={`https://github.com/${user.login}`}>{user.name}</a>
-            </h3>
-            <h2>
-              <a href={`https://github.com/${user.login}`}>{user.login}</a>
-            </h2>
-            <li>ID: {user.id}</li>
-            <li>{user.bio && user.bio.substring(0, 115)}</li>
-            <li>
-              <a href={`mailto:${user.email}`}>{user.email}</a>
-            </li>
-            <li>
-              <a href={`http://${user.blog.split("//").pop()}`} target="_blank">
-                {user.blog.split("//").pop()}
-              </a>
-            </li>
-            <h3>
-              <p>
-                <li>
-                  Repos:{" "}
-                  <a
-                    href={`https://github.com/${user.login}?tab=repositories`}
-                    target="_blank"
-                  >
-                    {" "}
-                    <b>{user.public_repos}</b>
-                  </a>
-                </li>
-                <li>
-                  Followers:{" "}
-                  <a
-                    href={`https://github.com/${user.login}?tab=followers`}
-                    target="_blank"
-                  >
-                    {user.followers}
-                  </a>
-                </li>
-                <li>
-                  Following:{" "}
-                  <a
-                    href={`https://github.com/${user.login}?tab=following`}
-                    target="_blank"
-                  >
-                    {user.following}
-                  </a>
-                </li>
-                <li>
-                  <a href={`https://github.com/${user.login}?tab=stars`}>
-                    Stars
-                  </a>
-                </li>
-              </p>
-            </h3>
-            <li>Since: {user.created_at.substring(0, 10)}</li>
-            <li>
-              <span className="gold">
-                {user.hireable ? "Available for hire" : ""}
-              </span>
-            </li>
-          </ul>
-          <div className="thumb" title="Give thumbs up">
-            &#128077;
-          </div>
+    <span className="user">
+        {user.login === coder.login && (
+            <div>
+                <div className="top-info1" />
+                <div className="top-info2" />
+                <div className="info">
+                    <ul>
+                        <h3>
+                            <a href={`https://github.com/${user.login}`}>
+                                {user.name}
+                            </a>
+                        </h3>
+                        <h2>
+                            <a href={`https://github.com/${user.login}`}>
+                                {user.login}
+                            </a>
+                        </h2>
+                        <li>ID: {user.id}</li>
+                        <li>{user.bio && user.bio.substring(0, 115)}</li>
+                        <li>
+                            <a href={`mailto:${user.email}`}>{user.email}</a>
+                        </li>
+                        <li>
+                            <a
+                                href={`http://${user.blog.split('//').pop()}`}
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                {user.blog.split('//').pop()}
+                            </a>
+                        </li>
+                        <h3>
+                            <p>
+                                <li>
+                                    Repos:{' '}
+                                    <a
+                                        href={`https://github.com/${
+                                            user.login
+                                        }?tab=repositories`}
+                                        target="_blank"
+                                        rel="noopener noreferrer">
+                                        {' '}
+                                        <b>{user.public_repos}</b>
+                                    </a>
+                                </li>
+                                <li>
+                                    Followers:{' '}
+                                    <a
+                                        href={`https://github.com/${
+                                            user.login
+                                        }?tab=followers`}
+                                        target="_blank"
+                                        rel="noopener noreferrer">
+                                        {user.followers}
+                                    </a>
+                                </li>
+                                <li>
+                                    Following:{' '}
+                                    <a
+                                        href={`https://github.com/${
+                                            user.login
+                                        }?tab=following`}
+                                        target="_blank"
+                                        rel="noopener noreferrer">
+                                        {user.following}
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href={`https://github.com/${
+                                            user.login
+                                        }?tab=stars`}>
+                                        Stars
+                                    </a>
+                                </li>
+                            </p>
+                        </h3>
+                        <li>Since: {user.created_at.substring(0, 10)}</li>
+                        <li>
+                            <span className="gold">
+                                {user.hireable ? 'Available for hire' : ''}
+                            </span>
+                        </li>
+                    </ul>
+                    <div className="thumb" title="Give thumbs up">
+                        <span role="img" aria-label="thumbs-up">
+                            &#128077;
+                        </span>
+                    </div>
 
-          <div className="position">{i + 1}</div>
-        </div>
-      </div>
-    )}
+                    <div className="position">{i + 1}</div>
+                </div>
+            </div>
+        )}
 
-    <img
-      id={coder.login}
-      style={{ width: "125px", height: "125px" }}
-      src={coder.avatar_url}
-      alt="avatar"
-    />
-  </span>
+        <img
+            id={coder.login}
+            style={{ width: '125px', height: '125px' }}
+            src={coder.avatar_url}
+            alt="avatar"
+        />
+    </span>
 );
 
 export default Coder;
